df5c1305fdabc1c7cd79e5d70c55e21b82db4186
Dylan0917 <46637586+Dylan0917@users.noreply.github.com>
2020-08-13 19:23:01 +0800

    0813提交

---
+++ SQL文件输出/sql文件输出out.txt.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Sqlout
+(
+  t1 VARCHAR2(2000)
+, t2 VARCHAR2(2000)
+)
+;
+
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
+INSERT INTO Sqlout(t1, t2) VALUES ('6','8');
